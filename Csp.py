d7862e78e3440a4edca3a3af289f4ce5f76c0719
Harckyl <lucien.morcrette@gmail.com>
2021-10-31 16:03:25 -0400

    csppy

--- Csp.py
+++ Csp.py
@@ -1,19 +1,47 @@
 class CSP():
+    Domain = [1,2,3,4,5,6,7,8,9]
+    def __init__(self,mode_voulu,board):
+        self.mode_voulu = mode_voulu
+        self.base_sudoku = board
+        self.assignement = board
+        self.unassigned_variable = [[board[i][j] == 0 for i in range(9)] for j in range(9)]
+        #self.arbre_constraint
+        #self.dictionnary_constraint
+
+    def launch_csp(self):
+        if (self.mode_voulu == 0):
+            #backtracking search
+            dosomething = 0
+        elif (self.mode_voulu == 1):
+            #MRV
+            dosomething = 0
+        elif (self.mode_voulu == 2):
+            #degree heuristic
+            dosomething = 0
+        elif (self.mode_voulu == 3):
+            #leastconstrainingvalue
+            dosomething = 0
+        elif (self.mode_voulu == 4):
+            #AC3
+            dosoemthing = 0
+
+        return 0
+
     def check_constraint(assignement, value, variable):
-    i = (int) (variable / 10)
-    j = variable % 10
-    for test in range(len(assignement)):
-        #check ligne
-        if (assignement[i][test] == value and i != test):
-            return False
-        #check colonne
-        if (assignement[test][j] == value and i != test):
-            return False
-        #check boite 
-        carre_x = i // 3
-        carre_y = j // 3
-        for oui in range(carre_y*3, carre_y*3 + 3):
-            for non in range(carre_x * 3, carre_x*3 + 3):
-                 if assignement[oui][non] == value and (oui, non) != (i, j):
-                     return False
-    return True
+        i = (int) (variable / 10)
+        j = variable % 10
+        for test in range(len(assignement)):
+            #check ligne
+            if (assignement[i][test] == value and i != test):
+                return False
+            #check colonne
+            if (assignement[test][j] == value and i != test):
+                return False
+            #check boite 
+            carre_x = i // 3
+            carre_y = j // 3
+            for oui in range(carre_y*3, carre_y*3 + 3):
+                for non in range(carre_x * 3, carre_x*3 + 3):
+                    if assignement[oui][non] == value and (oui, non) != (i, j):
+                        return False
+        return True
--- environment.py
+++ environment.py
@@ -1,3 +1,4 @@
+import Csp
 board = [
     [7,8,0,4,0,0,1,2,0],
     [6,0,0,0,7,5,0,0,9],
@@ -10,11 +11,8 @@ board = [
     [0,4,9,2,0,6,0,0,7]
 ]
 
-assignement = board 
 
-unassigned_variables = [[0 for i in range(9)] for i in range(9)]
 
-Domain = [1,2,3,4,5,6,7,8,9]
 
 #fonctionnement = variable = 1 ==> tableau[0][1]
 #variable = 10 ==> tableau[1][0]
@@ -31,41 +29,14 @@ def print_board(bo):
 
             if j == 8:
                 print(bo[i][j])
-                if bo[i][j] == 0:
-                    unassigned_variables[i][j] = 1 
-                else:
-                    unassigned_variables[i][j] = 0
+
 
             else:
                 print(str(bo[i][j]) + " ", end="")
-                if bo[i][j] == 0:
-                    unassigned_variables[i][j] = 1 
-                else:
-                    unassigned_variables[i][j] = 0
 
-print_board(assignement)
-print("")
-print_board(unassigned_variables)
+print_board(board)
 
-def check_constraint(assignement, value, variable):
-    i = (int) (variable / 10)
-    j = variable % 10
-    for test in range(len(assignement)):
-        #check ligne
-        if (assignement[i][test] == value and i != test):
-            return False
-        #check colonne
-        if (assignement[test][j] == value and i != test):
-            return False
-        #check boite 
-        carre_x = i // 3
-        carre_y = j // 3
-        for oui in range(carre_y*3, carre_y*3 + 3):
-            for non in range(carre_x * 3, carre_x*3 + 3):
-                 if assignement[oui][non] == value and (oui, non) != (i, j):
-                     return False
-    return True
  
 
-test = check_constraint(assignement, 9, variable)
-print(test)
+#test = check_constraint(assignement, 9, variable)
+#print(test)
